4080c2c695b4173b03c393e8c487b1cd7c4fce04
Scott Clasen <scott@heroku.com>
2017-01-09 10:14:24 -0800

    add python, tensorflow, keras, boto/s3, interpreter

--- conf/zeppelin-env.sh
+++ conf/zeppelin-env.sh
@@ -10,3 +10,7 @@ export SPARK_LOCAL_IP=$HEROKU_PRIVATE_IP
 export SPARK_PUBLIC_DNS=$HEROKU_DNS_DYNO_NAME
 export SPARK_HOME=/app/spark-home
 export SPARK_SUBMIT_OPTIONS='--jars=/app/spark-home/lib/hadoop-aws-shaded.jar,/app/spark-home/lib/hadoop-lzo.jar'
+export PYTHONHASHSEED=random
+export PYTHONHOME=/app/.heroku/python
+export PYTHONPATH=/app/
+export PYTHONUNBUFFERED=true
